bce0c6a12dd8b72bc7b31dc51d3d0dc978c21f14
Ejay Balina <ejbalina@gmail.com>
2025-09-13 14:35:37 +0800

    user temp

---
+++ mercedes_ois/user/USER TEMPLATE FRONTEND.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en" ng-app="mercedesApp">
+
+<head>
+    <meta charset="UTF-8">
+    <title>MERCEDEX</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+
+    <!-- FONTAWESOME -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.0/css/all.min.css">
+
+    <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.8.3/angular.min.js"></script>
+
+    <script src="../../mercedes_ois/admin/js/Sample.js"></script>
+
+    <!-- NASHIE CSS <3 -->
+    <link rel="stylesheet" href="../framework/CSS/NashieCss-Frontend.css">
+</head>
+
+<body class="bg-light" ng-controller="SampleController" ng-init="init()" >
+    <?php require_once '../framework/Components/mdx_user_header.php'; ?>
+
+    <div class="container" style="min-height: 100vh">
+        <!-- start coding here temporariliy
+         will update the UI later
+         add
+         - transaction related back-ends
+         - events fetching
+         - announcement fetching
+         - log out
+         - requests and incident reports
+         
+        -->
+    </div>
+
+
+    
+    <?php require_once '../framework/Components/mdx_footer.php'; ?>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous">
+        </script>
+
+</body>
+
+</html>
